e33f03dadefb95456260cd0dd6e74eac3c70b6c7
albertycliu <alert.ycliu@gmail.com>
2020-12-14 14:52:00 +0800

    123

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template
+#########Migrations#############
+app = Flask(__name__)
+from flask_cors import CORS
+CORS(app, resources=r'/*')
+@app.route('/test')
+def vue():
+
+    return render_template('test.html',**locals())
